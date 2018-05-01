f2225e36760ab0ff1f148ee879a681e4dc0113ad
Emil Agonoy <emil.agonoy2@gmail.com>
2018-05-01 16:31:08 -0700

    trying to make a 16byte data type

--- lib/lab10/inc/arraynumbers.h
+++ lib/lab10/inc/arraynumbers.h
@@ -6,8 +6,13 @@
 #define CMPE126S18_LABS_ARRAYNUMBERS_H
 
 
-class arraynumbers{
+class sxtnbyte{
 
-    int array[16];//
+    int array[4];//will hold a 16-byte number
+public:
+    sxtnbyte();
+    ~sxtnbyte();
+
+    sxtnbyte(unsigned);
 };
 #endif //CMPE126S18_LABS_ARRAYNUMBERS_H
--- lib/lab10/src/arraynumbers.cpp
+++ lib/lab10/src/arraynumbers.cpp
@@ -2,3 +2,10 @@
 // Created by Work on 5/1/2018.
 //
 #include "arraynumbers.h"
+
+sxtnbyte::sxtnbyte() {};
+sxtnbyte::~sxtnbyte() {};
+
+sxtnbyte::sxtnbyte(unsigned){
+
+};
